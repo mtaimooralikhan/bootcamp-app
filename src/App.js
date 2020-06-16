56989c42fa0c07660df7d5de6f5f20e48ba03e29
Taimoor ALI <33669528+mtaimooralikhan@users.noreply.github.com>
2020-06-17 03:46:01 +0500

    Update display message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Hello world from Taimoor Ali</div>
+      <div><h1>Hello world from Taimoor Ali</h1></div>
     </div>
   );
 }
